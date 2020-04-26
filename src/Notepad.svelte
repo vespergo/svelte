5a0044829f6d8c1ca2f6387d7185f9c9738fd432
Josh O'Bryan <vespergostorage@hotmail.com>
2020-04-25 20:56:46 -0700

    work: notepad.svelte

--- src/Notepad.svelte
+++ src/Notepad.svelte
@@ -5,29 +5,50 @@
   let id = 1234;
   let collection = [];
 
-  db.collection("notes")
-    .get()
-    .then(function(docs) {
-      let alldocs = [];
-      docs.forEach(element => {
-        let x = element.data();
-        alldocs.push(x);
-      });
-      collection = alldocs;
+  let notes = db
+    .collection("notes")
+    .orderBy("id", "desc")
+    .limit(10);
+
+  notes.get().then(function(docs) {
+    let alldocs = [];
+    docs.forEach(element => {
+      let x = element.data();
+      alldocs.push(x);
     });
+    collection = alldocs;
+  });
 
   function addToDb() {
-    let x = { id, text };
+    let x = { id: Date.now(), text };
     db.collection("notes").add(x);
-    collection.push(x);
+    collection.unshift(x);
     collection = collection;
     text = "";
   }
+
+  function checkEnter() {
+    if (event.key == "Enter") {
+      addToDb();
+    }
+  }
 </script>
 
-<textarea bind:value={text} />
-<button on:click={addToDb}>Add to Firebase</button>
+<style>
+  textarea {
+    width: 50%;
+    height: 20%;
+  }
+  .top {
+    display: flex;
+    justify-content: center;
+  }
+</style>
 
+<div class="top">
+  <textarea bind:value={text} on:keypress={checkEnter} />
+  <button on:click={addToDb}>Add to Firebase</button>
+</div>
 {#each collection as c}
   <p>id: {c.id} text: {c.text}</p>
 {/each}
